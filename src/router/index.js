4f85453aad81538b64f55a310f3214db536f3203
bobbysingh2005 <bobbysingh2005@gmail.com>
2024-05-05 20:16:09 +0530

    build

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import { HomeView, PageNotFound } from "../pages";
 
 export const router = createRouter({
-  base: "/web", // Adjust this to '/'
+  base: "/web/", // Adjust this to '/'
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "active",
   routes: [
